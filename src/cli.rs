4dda6daf5d04fe3bd13e7923d4cd1070c57897e6
Emerson MX <emersonmx@gmail.com>
2024-01-14 15:27:05 -0300

    Use parse_key_val from cookbook

--- src/cli.rs
+++ src/cli.rs
@@ -1,5 +1,6 @@
 use crate::config::Config;
 use clap::Parser;
+use std::error::Error;
 use std::{
     fs::{self, create_dir_all},
     path::PathBuf,
@@ -52,9 +53,19 @@ fn parse_output_path(value: &str) -> Result<PathBuf, String> {
     path.canonicalize().or(Err(format!("unable to resolve path '{value}'.")))
 }
 
-fn parse_input(value: &str) -> Result<Input, String> {
-    let (name, value) = value.split_once('=').ok_or("missing '=' in input.")?;
-    Ok((name.to_owned(), value.to_owned()))
+fn parse_key_val<T, U>(
+    s: &str,
+) -> Result<(T, U), Box<dyn Error + Send + Sync + 'static>>
+where
+    T: std::str::FromStr,
+    T::Err: Error + Send + Sync + 'static,
+    U: std::str::FromStr,
+    U::Err: Error + Send + Sync + 'static,
+{
+    let pos = s
+        .find('=')
+        .ok_or_else(|| format!("invalid KEY=value: no `=` found in `{s}`"))?;
+    Ok((s[..pos].parse()?, s[pos + 1..].parse()?))
 }
 
 #[derive(Parser, Debug)]
@@ -82,7 +93,7 @@ pub struct Cli {
     pub output_path: PathBuf,
 
     /// Inputs passed to the skeleton
-    #[arg(value_parser = parse_input)]
+    #[arg(value_parser = parse_key_val::<String, String>)]
     pub inputs: Vec<Input>,
 }
 
