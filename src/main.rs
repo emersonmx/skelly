20dac2d0901606bdaaaf2b1812b5ab8d7608f46e
Emerson MX <emersonmx@gmail.com>
2024-01-21 18:43:32 -0300

    Remove more unwrap

--- src/main.rs
+++ src/main.rs
@@ -10,23 +10,57 @@ use clap::Parser;
 use cli::Args;
 
 fn main() {
-    let args = Args::parse();
-    let use_input_terminal = std::io::stdin().is_terminal();
-    let use_output_terminal = std::io::stdout().is_terminal();
+    let response = handle_actions(
+        Args::parse(),
+        std::io::stdin().is_terminal(),
+        std::io::stdout().is_terminal(),
+    );
 
+    if response.is_err() {
+        std::process::exit(1);
+    }
+}
+
+fn handle_actions(
+    args: Args,
+    use_input_terminal: bool,
+    use_output_terminal: bool,
+) -> Result<(), String> {
     match (&args, use_input_terminal, use_output_terminal) {
         (Args { skeleton_config: Some(skeleton_config), .. }, true, true) => {
             let cleaned_inputs = validation::validate_inputs(
                 &args.inputs,
                 &skeleton_config.inputs,
             )
-            .unwrap();
+            .map_err(|error| {
+                let errors = error.0.iter().fold(String::new(), |acc, e| {
+                    let msg = match e {
+                        validation::ErrorType::MissingInput(name) => {
+                            format!("Missing input '{}'.", name)
+                        }
+                        validation::ErrorType::InvalidOption(key, value) => {
+                            format!(
+                                "Invalid option '{}' to input '{}'.",
+                                value, key
+                            )
+                        }
+                    };
+                    format!("{}{}\n", acc, msg)
+                });
+
+                eprint!("{errors}");
+
+                errors
+            })?;
             usecases::render_skeleton::execute(
                 &skeleton_config.template_directory,
                 &cleaned_inputs,
                 &args.output_path,
             )
-            .unwrap();
+            .map_err(|error| {
+                eprintln!("{}", error.0);
+                error.to_string()
+            })?;
         }
         (Args { skeleton_config: None, .. }, _, _) => {
             println!("NO SKELETON CONFIG!");
@@ -37,4 +71,6 @@ fn main() {
             println!("WAT?!");
         }
     }
+
+    Ok(())
 }
