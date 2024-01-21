6d982545f3c98f47e101bafefb3f8526ab925833
Emerson MX <emersonmx@gmail.com>
2024-01-21 16:23:31 -0300

    Make it work!

--- src/main.rs
+++ src/main.rs
@@ -1,29 +1,35 @@
 pub mod cli;
 pub mod config;
 pub mod renderer;
+pub mod usecases;
 pub mod validation;
+
+use std::io::IsTerminal;
 
 use clap::Parser;
 use cli::Args;
 
 fn main() {
     let args = Args::parse();
+    let use_input_terminal = std::io::stdin().is_terminal();
+    let use_output_terminal = std::io::stdout().is_terminal();
 
-    match &args {
-        Args { skeleton_config: Some(skeleton_config), .. } => {
-            let cleaned_inputs = validation::validate_inputs(
+    match (&args, use_input_terminal, use_output_terminal) {
+        (Args { skeleton_config: Some(skeleton_config), .. }, true, true) => {
+            usecases::render_skeleton::execute(
                 &args.inputs,
-                &skeleton_config.inputs,
-            );
-            eprintln!("skeleton_config = {:?}", skeleton_config);
-            eprintln!("output_path = {:?}", args.output_path);
-            eprintln!("inputs = {:?}", args.inputs);
-            eprintln!("cleaned_inputs = {:?}", cleaned_inputs);
+                &args.output_path,
+                skeleton_config,
+            )
+            .unwrap();
         }
-        Args { skeleton_config: None, .. } => {
+        (Args { skeleton_config: None, .. }, _, _) => {
             println!("NO SKELETON CONFIG!");
             eprintln!("output_path = {:?}", args.output_path);
             eprintln!("inputs = {:?}", args.inputs);
+        }
+        _ => {
+            println!("WAT?!");
         }
     }
 }
---
+++ src/usecases/render_skeleton.rs
@@ -0,0 +1,82 @@
+use std::{
+    fs,
+    path::{Path, PathBuf},
+};
+
+use walkdir::WalkDir;
+
+use crate::{config, renderer, validation};
+
+pub fn execute(
+    user_inputs: &[(String, String)],
+    output_path: &Path,
+    config: &config::Config,
+) -> Result<(), String> {
+    let cleaned_inputs =
+        validation::validate_inputs(user_inputs, &config.inputs).unwrap();
+
+    WalkDir::new(&config.template_directory)
+        .min_depth(1)
+        .into_iter()
+        .filter_map(|e| e.ok())
+        .map(|e| e.path().to_owned())
+        .filter(|p| !p.is_dir())
+        .for_each(|path| {
+            let rendered_template =
+                render_template(&path, &cleaned_inputs).unwrap();
+            let relative_path = strip_path_prefix(
+                &path,
+                config.template_directory.to_str().unwrap(),
+            )
+            .unwrap();
+            let rendered_relative_path =
+                render_path(&relative_path, &cleaned_inputs).unwrap();
+
+            write_temnplate(
+                &PathBuf::from(rendered_relative_path),
+                &rendered_template,
+                output_path,
+            );
+        });
+
+    Ok(())
+}
+
+fn strip_path_prefix(path: &Path, prefix: &str) -> Result<PathBuf, String> {
+    let relative_path = path.strip_prefix(prefix).map_err(|_| {
+        format!(
+            "Unable to strip template path '{}' from path '{}'",
+            prefix,
+            path.display()
+        )
+    })?;
+    Ok(PathBuf::from(relative_path))
+}
+
+fn render_template(
+    path: &Path,
+    inputs: &[(String, String)],
+) -> Result<String, String> {
+    let content =
+        fs::read_to_string(path).map_err(|_| "Unable to read template")?;
+    let rendered_content = renderer::render(&content, inputs)
+        .map_err(|_| "Unable to render template")?;
+    Ok(rendered_content)
+}
+
+fn render_path(
+    path: &Path,
+    inputs: &[(String, String)],
+) -> Result<String, String> {
+    let raw_path = path.to_str().ok_or("Unable to convert path to string.")?;
+    let rendered_path = renderer::render(raw_path, inputs)
+        .map_err(|_| "Unable to render path.")?;
+    Ok(rendered_path)
+}
+
+fn write_temnplate(path: &Path, content: &str, output_path: &Path) {
+    let output_path = output_path.join(path);
+    let output_directory = output_path.parent().unwrap();
+    fs::create_dir_all(output_directory).unwrap();
+    fs::write(&output_path, content).unwrap();
+}
