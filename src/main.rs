116dfc40e12da084cab72054318f49261860094d
Emerson MX <emersonmx@gmail.com>
2024-01-22 13:44:53 -0300

    Remove unreachable

--- src/main.rs
+++ src/main.rs
@@ -6,7 +6,7 @@ pub mod renderer;
 pub mod usecases;
 pub mod validation;
 
-use std::{io::IsTerminal, intrinsics::unreachable};
+use std::io::IsTerminal;
 
 use clap::Parser;
 use cli::Args;
@@ -41,7 +41,6 @@ fn handle_actions(
         (Args { skeleton_config: None, .. }, _, _) => {
             actions::stdin_to_stdout(&args)?
         }
-        _ => unreachable!(),
     }
 
     Ok(())
